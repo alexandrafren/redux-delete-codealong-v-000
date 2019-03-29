d58e1c798f7f7285b51f5e462ebacb58ffc55913
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-29 16:33:46 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -5,7 +5,7 @@ export default function manageTodo(state = {
     case 'ADD_TODO':
       return { todos: state.todos.concat(action.payload.text) };
     case 'DELETE_TODO':
-      return
+      return {todos: state.todos.filter(todo => !== action.payload)}
     default:
       return state;
   }
